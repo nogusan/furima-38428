f8c8317d45de9dae2ff08ca93fc8730ecfc283ff
nogusan <standing.nogusan@gmail.com>
2022-12-22 03:58:00 +0900

    ActiveHash_status作成

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,11 +1,11 @@
 class Item < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
 
   belongs_to :user
   has_one_attached :image
-
-  extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :categoly
+  belongs_to :status
 
   validates :categoly_id, numericality: { other_than: 1 , message: "can't be blank"} 
-
+  validates :status_id, numericality: { other_than: 1 , message: "can't be blank"}
 end
