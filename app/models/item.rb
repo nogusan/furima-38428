c53f493a9db3839b4827a5fd9a792eeab977d171
nogusan <standing.nogusan@gmail.com>
2022-12-22 04:13:03 +0900

    ActiveHash_delivery作成

--- app/models/item.rb
+++ app/models/item.rb
@@ -6,8 +6,10 @@ class Item < ApplicationRecord
   belongs_to :categoly
   belongs_to :status
   belongs_to :payer
+  belongs_to :delivery
 
   validates :categoly_id, numericality: { other_than: 1 , message: "can't be blank"} 
   validates :status_id, numericality: { other_than: 1 , message: "can't be blank"}
   validates :payer_id, numericality: { other_than: 1 , message: "can't be blank"}
+  validates :delivery_id, numericality: { other_than: 1 , message: "can't be blank"}
 end
