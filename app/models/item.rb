cabf8bf14e37e31a6807df461d0785068ab9d654
nogusan <standing.nogusan@gmail.com>
2022-12-22 04:17:48 +0900

    ActiveHash_place作成

--- app/models/item.rb
+++ app/models/item.rb
@@ -7,9 +7,11 @@ class Item < ApplicationRecord
   belongs_to :status
   belongs_to :payer
   belongs_to :delivery
+  belongs_to :place
 
   validates :categoly_id, numericality: { other_than: 1 , message: "can't be blank"} 
   validates :status_id, numericality: { other_than: 1 , message: "can't be blank"}
   validates :payer_id, numericality: { other_than: 1 , message: "can't be blank"}
   validates :delivery_id, numericality: { other_than: 1 , message: "can't be blank"}
+  validates :place_id, numericality: { other_than: 1 , message: "can't be blank"}
 end
