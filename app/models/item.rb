b3603445429c692496f01a38208921bcb74edf6d
nogusan <standing.nogusan@gmail.com>
2022-12-22 04:03:38 +0900

    ActiveHash_payer作成

--- app/models/item.rb
+++ app/models/item.rb
@@ -5,7 +5,9 @@ class Item < ApplicationRecord
   has_one_attached :image
   belongs_to :categoly
   belongs_to :status
+  belongs_to :payer
 
   validates :categoly_id, numericality: { other_than: 1 , message: "can't be blank"} 
   validates :status_id, numericality: { other_than: 1 , message: "can't be blank"}
+  validates :payer_id, numericality: { other_than: 1 , message: "can't be blank"}
 end
