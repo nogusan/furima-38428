b6a885c2a17c5fcc7080b64f673bca7ab3159ab1
nogusan <standing.nogusan@gmail.com>
2022-12-21 22:10:16 +0900

    ActiveHash_categoly作成

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,6 @@
 class Item < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :categoly
+
+  validates :categoly_id, numericality: { other_than: 1 , message: "can't be blank"} 
 end
