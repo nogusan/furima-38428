7d0f7b091b142f16098d90445114d211162fb3b7
nogusan <standing.nogusan@gmail.com>
2022-12-22 21:21:39 +0900

    controller設定

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,31 @@
 class ItemsController < ApplicationController
+  before_action :move_to_index, except: :index 
+
   def index
+  end
+
+  def new
+    @item = Item.new
+  end
+
+  def create
+    @item = Item.create(item_params)
+    if @item.save
+      redirect_to root_path
+    else  
+      render :new
+    end
+  end
+
+  private
+
+  def item_params
+    params.require(:item).permit(:item_name, :content, :price, :categoly_id, :status_id, :payer_id, :delivery_id, :place_id, :image).merge(user_id: current_user.id) 
+  end
+
+  def move_to_index
+    unless user_signed_in?
+      redirect_to action: :index
+    end
   end
 end
