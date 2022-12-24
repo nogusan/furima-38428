f392ce87a8250eaa517cbf40c2c6a46a97c00ce9
nogusan <standing.nogusan@gmail.com>
2022-12-24 11:28:16 +0900

    テストコード修正1

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -59,7 +59,7 @@ RSpec.describe Item, type: :model do
         expect(@item.errors.full_messages).to include("Price can't be blank")
       end
       it '価格は、¥10000000以上は保存不可であること' do
-        @item.price = Faker::Number.number(10_000_000)
+        @item.price = 10000000
         @item.valid?
         expect(@item.errors.full_messages).to include('Price is out of setting range')
       end
