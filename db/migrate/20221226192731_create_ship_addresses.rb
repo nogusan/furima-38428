244b839db5259bb0e9f4d04380f759a532d434c4
nogusan <standing.nogusan@gmail.com>
2022-12-27 04:37:54 +0900

    ship_address_migration作成

--- db/migrate/20221226192731_create_ship_addresses.rb
+++ db/migrate/20221226192731_create_ship_addresses.rb
@@ -2,6 +2,13 @@ class CreateShipAddresses < ActiveRecord::Migration[6.0]
   def change
     create_table :ship_addresses do |t|
 
+      t.string :post, null: false
+      t.string :city, null: false
+      t.string :line, null: false
+      t.string :build_name
+      t.string :tell, null: false
+      t.references :order, null: false, foreign_key: true
+      t.integer :place_id, null: false
       t.timestamps
     end
   end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_12_26_124839) do
+ActiveRecord::Schema.define(version: 2022_12_26_192731) do
 
   create_table "active_storage_attachments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
@@ -57,6 +57,19 @@ ActiveRecord::Schema.define(version: 2022_12_26_124839) do
     t.index ["user_id"], name: "index_orders_on_user_id"
   end
 
+  create_table "ship_addresses", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "post", null: false
+    t.string "city", null: false
+    t.string "line", null: false
+    t.string "build_name"
+    t.string "tell", null: false
+    t.bigint "order_id", null: false
+    t.integer "place_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["order_id"], name: "index_ship_addresses_on_order_id"
+  end
+
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -79,4 +92,5 @@ ActiveRecord::Schema.define(version: 2022_12_26_124839) do
   add_foreign_key "items", "users"
   add_foreign_key "orders", "items"
   add_foreign_key "orders", "users"
+  add_foreign_key "ship_addresses", "orders"
 end
